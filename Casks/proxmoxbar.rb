94072b90409d846e9fd83ad609a52e7ffd4d2767
koproductions-code <konstantin@koproductions.dev>
2026-03-12 15:10:21 +0100

    Added version

--- Casks/proxmoxbar.rb
+++ Casks/proxmoxbar.rb
@@ -1,4 +1,7 @@
 cask "proxmoxbar" do
+  version "2.0.4"
+  sha256 "e234fb29f28075be8303a4cd7d540e7c4cbcaa574a157a90a03273d6827e7837"
+
   url "https://github.com/ryzenixx/proxmoxbar-macos/releases/download/v#{version}/ProxmoxBar.dmg"
   name "ProxmoxBar"
   desc "Native macOS menu bar app for monitoring and controlling Proxmox VE resources."
